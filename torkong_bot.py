a86fbfc134d86868b0f62d333a4b94e9f8f64154
Konstantin Tornovskiy <coolship@yandex.ru>
2020-06-30 11:31:06 +0300

    Removed initial message

--- torkong_bot.py
+++ torkong_bot.py
@@ -1,13 +1,21 @@
-import os
 import datetime as dt
+import os
+import time
+import logging
+
 import requests
 import telegram
-import time
-from dotenv import load_dotenv
-from cryptography.x509 import load_der_x509_crl, load_pem_x509_crl
 from cryptography.hazmat.backends import default_backend
+from cryptography.x509 import load_der_x509_crl, load_pem_x509_crl
+from dotenv import load_dotenv
 
 load_dotenv()
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s %(name)s: %(message)s',
+    datefmt='%d-%b-%y %H:%M:%S',
+    filename='torkong_bot.log',
+    level=logging.INFO
+)
 
 WATCH_PERIOD = int(os.getenv('WATCH_PERIOD'))
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
@@ -15,19 +23,24 @@ BASE_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 CRL_URLS = os.getenv('CRL_URLS_LIST').split(' ')
 CRL_OVERLAP_TIME = int(os.getenv('CRL_OVERLAP_TIME'))
 
 
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']
-    if homework['status'] != 'approved':
-        verdict = 'Code reviewer found errors in homework.'
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
+    if homework_status and homework_name:
+        if homework_status == 'rejected':
+            verdict = 'Code reviewer found errors in homework.'
+        elif homework_status == 'approved':
+            verdict = 'It is accepted! You can start next lesson!'
+        else:
+            raise ValueError('Bad "status" key value')
+        return f'Homework "{homework_name}" is checked!\n\n{verdict}'
     else:
-        verdict = 'It is accepted! You can start the next lesson!'
-    return f'Homework "{homework_name}" is checked!\n\n{verdict}'
+        raise KeyError('No "homework_name" and/or "status" key in argument.')
 
 
 def get_homework_statuses(current_timestamp):
@@ -57,6 +70,7 @@ def check_crl_status(crl_urls):
             crl_status['server_errors'].append(
                 f'CRL {crl_file} getting error - {error}'
             )
+            logging.error(f'CRL {crl_file} getting error - {error}')
             continue
         try:
             crl = load_der_x509_crl(response.content,
@@ -71,50 +85,54 @@ def check_crl_status(crl_urls):
     return crl_status
 
 
-def parse_crl_status(crl_errors):
+def parse_crl_status(errors):
     message_text = ''
-    if crl_errors['server_errors']:
+    server_errors = errors.get('server_errors')
+    crl_errors = errors.get('crl_errors')
+    if server_errors:
         message_text = 'Infrastructure errors: '
-        for error in crl_errors['server_errors']:
+        for error in server_errors:
             message_text += f'\n{error}'
-    if crl_errors['crl_errors']:
+    if crl_errors:
         text = "\nIt's time to update crl: "
-        for crl_file in crl_errors['crl_errors']:
-            if crl_file == crl_errors['crl_errors'][-1]:
+        for crl_file in crl_errors:
+            if crl_file == crl_errors[-1]:
                 message_text += f'{text}{crl_file}.'
             else:
                 message_text += f'{text}{crl_file}, '
     return message_text
 
 
-def send_message(message):
+def send_message(message, bot):
     return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
+    logging.info('BOT was started')
     current_timestamp = int(time.time())
-    send_message('TorKonG BOT now back in work!!!')
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
                 send_message(
-                    parse_homework_status(new_homework.get('homeworks')[0])
+                    parse_homework_status(new_homework.get('homeworks')[0]),
+                    bot
                 )
             current_timestamp = new_homework.get('current_date')
 
             is_crl_updated = check_crl_status(CRL_URLS)
             if is_crl_updated['server_errors'] or is_crl_updated['crl_errors']:
-                send_message(parse_crl_status(is_crl_updated))
+                send_message(parse_crl_status(is_crl_updated), bot)
 
             time.sleep(WATCH_PERIOD)
 
         except KeyboardInterrupt:
-            print('You stopped BOT with keyboard!!!')
+            logging.info('BOT stopped with keyboard')
             break
         except Exception as error:
-            print(f'BOT is dropped down with error: {error}')
+            logging.error(f'BOT is dropped down with error: {error}')
             time.sleep(10)
             continue
 
